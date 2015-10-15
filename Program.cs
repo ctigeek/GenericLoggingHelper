6d5c5b75a454fb12d87efd565970489d3b98712b
Steven Swenson <steven.swenson@rackspace.com>
2015-10-15 15:34:27 -0400

    Change from dll to console for testing. Change NLog from 4.1.2 to 4.0.0. Change CLR from 4.5.2 to 4.5.0.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourNamespace.Logging;
+
+namespace GenericLoggingHelperTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var logger = LogManager.GetLogger(typeof (Program));
+                logger.Debug("hi");
+                logger.Error("err");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
